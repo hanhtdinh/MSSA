c8959a8e1af420a9b89cdbdf20b476c435d835e5
Hanh Dinh <79621530+hanhtdinh@users.noreply.github.com>
2021-05-05 22:06:31 -0700

    Add files via upload

---
+++ StudentInfoApp/StudentInfoApp/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoApp
+{
+    class Student
+    {
+        private int _studentId;
+        private string _studentFname;
+        private string _studentLname;
+        private char _studentGrade;
+
+        public Student()
+        {
+
+        }
+        public Student(int studentId, string studentFname, string studentLname, char studentGrade)
+        {
+            this.studentId = studentId;
+            this.studentFname = studentFname;
+            this.studentLname = studentLname;
+            this.studentGrade = studentGrade;
+
+        }
+        public int studentId
+        {
+            get { return this._studentId; }
+            set { this._studentId = value; }
+        }
+
+        public string studentFname
+        {
+            get { return this._studentFname; }
+            set { this._studentFname = value; }
+        }
+
+        public string studentLname
+        {
+            get { return this._studentLname; }
+            set { this._studentLname = value; }
+        }
+
+        public char studentGrade
+        {
+            get { return this._studentGrade; }
+            set
+            {
+                if (value > 0)
+                {
+                    this._studentGrade = value;
+                }
+            }
+        }
+}
---
+++ StudentInfoApp/StudentInfoApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Student Info App.");
+            Console.WriteLine("**************************************************************");
+            Student student1 = new Student();
+            Console.WriteLine("Please enter student ID.");
+            student1.studentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter student's first name.");
+            Console.ReadLine();
+            Console.WriteLine("Please enter student's last name.");
+            Console.ReadLine();
+            Console.WriteLine("Please enter student's letter grade.");
+            Console.ReadLine();
+
+        }
+    }
+}

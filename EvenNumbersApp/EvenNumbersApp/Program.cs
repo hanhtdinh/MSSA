433e9727619d54fef34d6ebf14cb19d0c7e197d8
Hanh Dinh <79621530+hanhtdinh@users.noreply.github.com>
2021-05-05 22:05:43 -0700

    Add files via upload

---
+++ EvenNumbersApp/EvenNumbersApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbersApp
+{
+    class Program
+    {
+        static void ReturnEvenNumbers() //creating a method
+        {
+            for(int i=1; i<=100; i++) //iterate from this range, ++ means increasing
+            {
+                if(i%2==0) //checks if remainder is = to 0
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's return all even numbers from 0-100: \t");
+
+            ReturnEvenNumbers();
+            
+            
+            Console.ReadKey();
+        }
+
+        
+    }
+}
